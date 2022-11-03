e29076be0d72d1217cc3b643f6067413db35d29f
Mourice-Adero <aderomourice7@gmail.com>
2022-11-03 11:02:41 +0300

    initial commmit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import header from "./components/header";
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return <div>
+    <Header />
+  </div>;
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='w-full bg-white p-5'>header</div>
+  )
+}
+
+export default Header
