6075cdbd65be6b134172effcbd2a9075490d4bbc
Mourice-Adero <aderomourice7@gmail.com>
2022-11-11 12:35:47 +0300

    added nav icon

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 import header from "./components/header";
 import "./App.css";
 import Header from "./components/Header";
+import Hero from "./components/HeroSection";
 
 function App() {
   return <div>
     <Header />
+    <Hero />
   </div>;
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import {AiOutlineClose} from 'react-icons/ai'
+import {BiSearch} from 'react-icons/bi'
+import {BsPerson} from 'react-icons/bs'
+import {HiOutlineMenuAlt4} from 'react-icons/hi'
 
 function Header() {
   return (
-    <div className='w-full bg-white p-5'>header</div>
-  )
+    <div className="w-full bg-white p-5 text-black flex justify-between shadow-md fixed">
+      <div>
+        <h1 className="font-bold text-2xl">MegaMind</h1>
+      </div>
+      <ul className="gap-2 hidden md:flex md:text-xl">
+        <li>Home</li>
+        <li>About</li>
+        <li>Articles</li>
+        <li>Cartegories</li>
+        <li>Items</li>
+      </ul>
+      <div className="hidden md:flex gap-2 p-2">
+        <BiSearch className="text-xl"/>
+        <BsPerson className="text-xl"/>
+      </div>
+      <div className="md:hidden">
+        <AiOutlineClose />
+      </div>
+      <div className="md:hidden">
+        <HiOutlineMenuAlt4/>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
